6c93c56ce1edbf7162329c4fc375fd05113092ef
umair <cyumair@hotmail.com>
2022-05-07 20:19:25 +0500

    updates on mint

---
+++ src/layouts/Home/Teaser/Teaser.js
@@ -0,0 +1,13 @@
+
+const Teaser = () => {
+    return (
+        <div className="teaser">
+            <iframe class="responsive-youtube" 
+                src="https://www.youtube.com/embed/OjXkeHHPTzQ" 
+                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+        </div>
+    );
+}
+
+export default Teaser;
